6afe0f92cf3ed061cd7f65531a7beaf92a8a3fe9
vibincholayil <vibinnath.c@gmail.com>
2025-09-24 14:15:50 -0700

    update

--- AZURE-environment/provider.tf
+++ AZURE-environment/provider.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
-  features {}
+  features = {}
+  tenant_id       = "2b32b1fa-7899-482e-a6de-be99c0ff5516"
+  subscription_id = "3a72be92-287b-4f1e-840a-5e3e71100139"
 }
 
 terraform {
