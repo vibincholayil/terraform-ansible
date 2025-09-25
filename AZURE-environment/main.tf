35e4a0ef4dd9b988a84678b71c71692b85d9b156
vibincholayil <vibinnath.c@gmail.com>
2025-09-25 01:25:30 -0700

    update

--- AZURE-environment/main.tf
+++ AZURE-environment/main.tf
@@ -1,6 +1,15 @@
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group
-  location = var.location
+provider "azurerm" {
+  features {}
+  subscription_id = "3a72be92-287b-4f1e-840a-5e3e71100139"
+}
+
+# -----------------------
+# Resource Group
+# -----------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-rg"
+  location = "East US"
+
   tags = {
     Env      = "Dev"
     costPlan = "1y"
@@ -8,94 +17,146 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
+# -----------------------
 # Virtual Network
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet-team-a"
+# -----------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Env      = "Dev"
+    costPlan = "1y"
+    Owner    = "Terraform"
+  }
 }
 
+# -----------------------
 # Subnet
-resource "azurerm_subnet" "main" {
-  name                 = "subnet-team-a"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+# -----------------------
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "tf-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Network Security Group
-resource "azurerm_network_security_group" "main" {
-  name                = "nsg-team-a"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+# -----------------------
+# Network Security Group with SSH & HTTP
+# -----------------------
+variable "nsg_rules" {
+  default = [
+    {
+      name                       = "SSH"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range           = "*"
+      destination_port_range      = "22"
+      source_address_prefix       = "*"
+      destination_address_prefix  = "*"
+    },
+    {
+      name                       = "HTTP"
+      priority                   = 200
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range           = "*"
+      destination_port_range      = "80"
+      source_address_prefix       = "*"
+      destination_address_prefix  = "*"
+    }
+  ]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "tf-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  dynamic "security_rule" {
+    for_each = var.nsg_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }
   }
 
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  tags = {
+    Env      = "Dev"
+    costPlan = "1y"
+    Owner    = "Terraform"
   }
 }
 
-# Network Interface
-resource "azurerm_network_interface" "main" {
-  name                = "nic-team-a"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
+# -----------------------
+# Public IP
+# -----------------------
+resource "azurerm_public_ip" "vm_ip" {
+  name                = "tf-vm-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# -----------------------
+# NIC
+# -----------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "tf-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.main.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id           = azurerm_public_ip.main.id
+    public_ip_address_id          = azurerm_public_ip.vm_ip.id
   }
 
-  //network_security_group_id = azurerm_network_security_group.main.id
+
+  tags = {
+    Env      = "Dev"
+    costPlan = "1y"
+    Owner    = "Terraform"
+  }
 }
 
-# Public IP
-resource "azurerm_public_ip" "main" {
-  name                = "pip-team-a"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
-}
-
-# Virtual Machine
-resource "azurerm_linux_virtual_machine" "main" {
-  name                = "vm-team-a"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
+# -----------------------
+# Linux VM
+# -----------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "tf-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = var.vm_admin_username
+  admin_username      = "azureuser"
+
   network_interface_ids = [
-    azurerm_network_interface.main.id
+    azurerm_network_interface.nic.id,
   ]
 
-  disable_password_authentication = true
-
-  # SSH Key
   admin_ssh_key {
-    username   = var.vm_admin_username
-    public_key = file("~/.ssh/azure_id_rsa.pub")
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
@@ -105,16 +166,29 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-lts"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
-
-  computer_name = "vm-team-a"
 
   tags = {
     Env      = "Dev"
     costPlan = "1y"
     Owner    = "Terraform"
   }
+}
+
+# -----------------------
+# Outputs
+# -----------------------
+output "private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.vm_ip.ip_address
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
 }
